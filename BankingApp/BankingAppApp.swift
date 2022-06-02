2b7e77124eff6468ee50bb738becc4093c911f2a
Asadullah Behlim <asadullahbehlim@gmail.com>
2022-06-02 09:48:21 +0530

    Initial Commit

---
+++ BankingApp/BankingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingAppApp.swift
+//  BankingApp
+//
+//  Created by Asadullah Behlim on 02/06/22.
+//
+
+import SwiftUI
+
+@main
+struct BankingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
